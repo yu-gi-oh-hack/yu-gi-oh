7b0298a6911c1dd3d51465f58cb6f36fae9d732a
Dienul <haqdienul@gmail.com>
2019-05-16 22:39:06 +0700

    start page

--- client/src/router.js
+++ client/src/router.js
@@ -20,6 +20,23 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/startgame',
+      name: 'start-game',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/start-game.vue')
+    },
+    ,
+    {
+      path: '/roomgame',
+      name: 'roomgame',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/room-game.vue')
+    },
   ]
 })
---
+++ client/src/views/room-game.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 style="diplay:flex; justify-content: center">ROOM</h1>
+    </div>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Room</th>
+      <th scope="col">Qty</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
