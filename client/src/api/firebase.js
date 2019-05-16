37e28a08fca873da9f384561f08a5f87385c8363
qoyyima22 <qoyyimafiassalam@gmail.com>
2019-05-17 01:30:33 +0700

    amankan apikey

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -1,31 +1,10 @@
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#config-web-app -->
-
-// <script>
-//   // Your web app's Firebase configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyA_oRr0c57-huvDfysn9RRWL5Nii50uSq0",
-//     authDomain: "yu-gi-oh-dd7d2.firebaseapp.com",
-//     databaseURL: "https://yu-gi-oh-dd7d2.firebaseio.com",
-//     projectId: "yu-gi-oh-dd7d2",
-//     storageBucket: "yu-gi-oh-dd7d2.appspot.com",
-//     messagingSenderId: "579278828065",
-//     appId: "1:579278828065:web:ea0a85774536406d"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-// </script>
-
 const config = {
-    apiKey: "AIzaSyA_oRr0c57-huvDfysn9RRWL5Nii50uSq0",
-    authDomain: "yu-gi-oh-dd7d2.firebaseapp.com",
-    databaseURL: "https://yu-gi-oh-dd7d2.firebaseio.com",
-    projectId: "yu-gi-oh-dd7d2",
-    storageBucket: "yu-gi-oh-dd7d2.appspot.com",
-    messagingSenderId: "579278828065",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: 'yu-gi-oh-dd7d2',
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
   };
   
   const firebase = require('firebase');
